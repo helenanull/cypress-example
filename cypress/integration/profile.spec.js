342524fa32a3407b89f89569f3f180c486e3a5a9
helenanull <helena.github@gmail.com>
2021-03-31 23:01:27 +0300

    better test for favorites

--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -21,8 +21,29 @@ describe('Profile page', () => {
 
     it('can see favorited articles', function () {
         const apiUrl = Cypress.env('apiUrl')
-        // we already test adding favourites in home spec
-        // here we can mock my favourited articles list
+        // we already test adding favorite in home spec, we know it works
+        // here we can use API to favourite an article (then we have tested it E2E)
+        // create a new article
+        cy.createArticle().then((link) => {
+            // and then add newly created article to favorites
+            cy.request({
+                method: 'POST',
+                url: `${apiUrl}/articles/${link}/favorite`,
+                headers: {
+                    authorization: `Token ${window.localStorage.getItem('jwtToken')}`
+                }
+            })
+        })
+        cy.visit(`/@${this.username}`)
+        cy.get(profile.favouritedArticlesTab).click()
+        cy.get(profile.articles).should('be.visible')
+            .and('have.length', 1)
+            .and('contain', 'My Cypress article')
+    })
+
+    it('can see favorited articles - mock response', function () {
+        const apiUrl = Cypress.env('apiUrl')
+        // example how to mock favourited articles list
         cy.intercept(`${apiUrl}/articles?favorited=${this.username}&limit=5&offset=0*`, {
             fixture: 'favorited_list'
         })
